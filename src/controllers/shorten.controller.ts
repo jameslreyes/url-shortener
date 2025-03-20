110aff08ea4db8e4b905a70cadade8fab25f3e96
James Reyes <jamesleoreyes@gmail.com>
2025-03-20 00:39:22 -0400

    Initial commit

---
+++ src/controllers/shorten.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { supabase } from "../services/supabase.service";
+import { UrlController } from "../types/Controllers";
+
+export const urlController: UrlController = {
+  shorten: async (req: Request, res: Response): Promise<void> => {
+    const { longUrl } = req.body;
+    const response = await supabase.shortenUrl(longUrl);
+    console.log(`shorten.controller.ts --> shorten() --> Shortened URL [${longUrl}] to [${response}]`)
+    res.status(200).json(response);
+  },
+
+  redirect: async (req: Request, res: Response): Promise<void> => {
+    const { shortCode } = req.params;
+    const longUrl = await supabase.getLongUrl(shortCode);
+
+    if (!longUrl) {
+      console.error(`shorten.controller.ts --> redirect() --> Short URL not found for short code [${shortCode}]`)
+      res.status(404).json({ error: "Short URL not found" })
+      return;
+    }
+
+    await supabase.trackClick(shortCode);
+    console.log(`shorten.controller.ts --> redirect() --> Redirecting to [${longUrl}]`)
+    res.redirect(longUrl);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express'
+import { env } from './env';
+import { urlController } from './controllers/shorten.controller';
+
+const app = express();
+app.use(express.json());
+
+app.post('/shorten', urlController.shorten);
+app.get('/:shortCode', urlController.redirect);
+
+app.get('/', (req: Request, res: Response) => {
+  console.log(`Received request`)
+  res.status(200).json({
+    success: true,
+    message: "API is working!"
+  })
+});
+
+app.listen(env.port, () => {
+  console.log(`Server is listening on port ${env.port}`)
+});
